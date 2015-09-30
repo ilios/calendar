8ef93bd59e6e075f206ea2ea9908d09acc1500de
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-09-30 01:17:21 -0700

    Auto-scroll to 8am in week claendar

--- addon/components/ilios-calendar-week.js
+++ addon/components/ilios-calendar-week.js
@@ -2,13 +2,19 @@ import Ember from 'ember';
 import moment from 'moment';
 import layout from '../templates/components/ilios-calendar-week';
 
-const {computed} = Ember;
+const {computed, run} = Ember;
 
 export default Ember.Component.extend({
+  classNames: ['ilios-calendar-week'],
   layout: layout,
   date: null,
   calendarEvents: [],
   weekOf: computed('selectedDate', function(){
     return moment(this.get('selectedDate')).startOf('week').format('MMMM Do YYYY');
   }),
+  didInsertElement(){
+    run.next(() => {
+      this.$(".el-calendar .week").scrollTop(500);
+    });
+  },
 });
