9b9ad843282f95cd49ab61deced50e9986bc887e
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-10-11 21:05:27 -0700

    Add file type icons to single event learning materials

--- addon/components/ilios-calendar-single-event-learningmaterial-list.js
+++ addon/components/ilios-calendar-single-event-learningmaterial-list.js
@@ -1,7 +1,31 @@
 import Ember from 'ember';
 import layout from '../templates/components/ilios-calendar-single-event-learningmaterial-list';
 
+const {computed} = Ember;
+
 export default Ember.Component.extend({
   layout: layout,
   learningMaterials: [],
+  proxiedLearningMaterials: computed('learningMaterials.[]', function(){
+    return this.get('learningMaterials').map(lm => {
+      let icon;
+      if(lm.type === 'link' || lm.type === 'citation'){
+        icon = 'fa-link';
+      } else {
+        icon = 'fa-file';
+        if(lm.mimetype.search(/pdf/) !== -1){
+          icon = 'fa-file-pdf-o';
+        }
+        if(lm.mimetype.search(/ppt/) !== -1){
+          icon = 'fa-file-powerpoint-o';
+        }
+      }
+      
+      return {
+        title: lm.title,
+        url: lm.url,
+        icon
+      };
+    });
+  })
 });
